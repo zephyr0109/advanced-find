1a10dc9c7def35f721ed1ffbff522875540a4c2a
swatch <swatch.code@gmail.com>
2012-02-09 21:41:09 +0800

    Release v0.8.3

--- src/advancedfind/config_ui.py
+++ src/advancedfind/config_ui.py
@@ -46,8 +46,9 @@ import os.path
 class ConfigUI(object):
 	def __init__(self, plugin):
 		self._plugin = plugin
-		self._instance = self._plugin.get_instance()
-		self._window = self._instance._window
+		#self._instance = self._plugin.get_instance()
+		#self._window = self._instance.window
+		self._instance, self._window = plugin.get_instance()
 	
 		#Set the Glade file
 		gladefile = os.path.join(os.path.dirname(__file__),"config.glade")
