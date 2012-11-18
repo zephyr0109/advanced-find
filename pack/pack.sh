26e0a4f5f0834193ff8d2db8e29b0e068c4874a5
swatch <swatch.code@gmail.com>
2012-11-18 14:48:07 +0800

    Fix : issue80. New package archive script.

--- pack/pack.sh
+++ pack/pack.sh
@@ -3,13 +3,23 @@
 
 PLUGIN_NAME="advanced_find"
 
-ver_str=$(zenity --entry --title="Version" --text="Enter the version code : " --entry-text="")
-if [ "${ver_str}" == "" ]; then
-	exit
-fi
+#ver_code=$(zenity --entry --title="Version" --text="Enter the version code : " --entry-text="")
+#if [ "${ver_code}" == "" ]; then
+	#exit
+#fi
 
-tar -zcv -f ${PLUGIN_NAME}'-'${ver_str}.tar.gz ../src/*
+read -p "Enter the plugin version code : " ver_code
 
+PACK_NAME=${PLUGIN_NAME}'-'${ver_code}
 
-
+# create temp folder for plugin
+mkdir ${PACK_NAME}
+# copy plugin files to temp folder
+cp -r ../src/*	${PACK_NAME}
+# create plugin package
+tar -zcv -f ${PACK_NAME}.tar.gz ${PACK_NAME}/*
+# remove temp files
+rm -rf ${PACK_NAME}/*
+# remove temp folder
+rmdir ${PACK_NAME}
 
